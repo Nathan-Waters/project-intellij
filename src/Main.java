dbe2dee6474cf0bc40ae2bf94e29d7082637f1a4
NathanWaters <nathanwaters94@gmail.com>
2024-01-05 08:57:01 -0800

    changed hello world

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       System.out.println("hello world");
+       printMessage("hello World");
 
        printHeader();
        System.out.println("New thing");
